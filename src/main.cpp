ed25276ceea79f8e0b19b876b02a73a9f639988b
Jimmy Envall <jimmyenvall@gmail.com>
2018-04-13 13:59:11 +0200

    Demo, info and command for creating ply of freiburg data.

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,32 @@
 #include <Eigen/Geometry>
 
 #include <GLFW/glfw3.h>
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
+#include <imgui/imgui.h>
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
 Mesh<double> mesh;
+Eigen::MatrixXd P, P1, P2;
 
 template <typename T>
-void generate_example_point_cloud(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P)
+void split_point_cloud(const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P1, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P2, const Eigen::Matrix<int, Eigen::Dynamic, 1>& id1, Eigen::Matrix<int, Eigen::Dynamic, 1>& id2)
+{
+  P1.resize(id1.rows(), 3);
+  P2.resize(id2.rows(), 3);
+  
+  for (int i = 0; i < id1.rows(); ++i)
+    P1.row(i) = P.row(id1(i));
+  
+  for (int i = 0; i < id2.rows(); ++i)
+    P2.row(i) = P.row(id2(i));
+}
+
+template <typename T>
+void generate_example_point_cloud1(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P)
 {
   const int size = 120;
   P.resize(size*size,3);
@@ -53,12 +70,57 @@ void generate_example_point_cloud(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynami
   }
 }
 
-bool callback_key_down(igl::opengl::glfw::Viewer &viewer, unsigned char /*key*/, int /*modifier*/)
+template <typename T>
+void generate_example_point_cloud2(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& P, Eigen::Matrix<int, Eigen::Dynamic, 1>& id1, Eigen::Matrix<int, Eigen::Dynamic, 1>& id2)
 {
-  mesh.iterate();
-  viewer.data().set_mesh(mesh.vertices(), mesh.faces());
-  viewer.data().compute_normals();
-  viewer.data().invert_normals = true;
+  const int size = 120;
+  P.resize(size*size,3);
+  id1.resize(size*size / 2);
+  id2.resize(size*size / 2);
+  
+  int cntr1 = 0, cntr2 = 0;
+  for (int x = 0; x < size; ++x)
+  {
+    for (int y = 0; y < size; ++y)
+    {
+      T height;
+      
+      if (y / (size / 5) % 2 == 0)
+        height = 15;
+      else
+        height = 0;
+        
+      Eigen::Matrix<T, 1, 3> row(T(x), height, T(y));
+      P.row(x + y*size) = row;
+      
+      if (x < size / 2)
+        id1(cntr1++) = x + y*size;
+      else
+        id2(cntr2++) = x + y*size;
+    }
+  }
+  
+}
+
+bool callback_key_down(igl::opengl::glfw::Viewer &viewer, unsigned char key, int /*modifier*/)
+{
+  if (key == '1')
+  {
+    mesh.iterate();
+    viewer.data().set_mesh(mesh.vertices(), mesh.faces());
+    viewer.data().compute_normals();
+    viewer.data().invert_normals = true;
+  }
+  
+  if (key == '2')
+  {
+    viewer.data().clear();
+    mesh.set_target_point_cloud(P2);
+    viewer.data().add_points(P2, Eigen::RowVector3d(0.f,0.7f,0.7f));
+    viewer.data().set_mesh(mesh.vertices(), mesh.faces());
+    viewer.data().compute_normals();
+    viewer.data().invert_normals = true;
+  }
   
   return true;
 }
@@ -73,11 +135,12 @@ int main(int argc, char **/*argv*/) {
       return EXIT_SUCCESS;
     }
     
-    Eigen::MatrixXd P;
-    generate_example_point_cloud(P);
+    Eigen::VectorXi id1, id2;
+    generate_example_point_cloud2(P, id1, id2);
+    split_point_cloud(P, P1, P2, id1, id2);
     
     mesh.align_to_point_cloud(P); // Resets the mesh everytime it is called
-    mesh.set_target_point_cloud(P); // Use this to solve for the height field
+    mesh.set_target_point_cloud(P1);
     
     igl::opengl::glfw::Viewer viewer;    
     viewer.callback_key_down = callback_key_down;
@@ -85,10 +148,19 @@ int main(int argc, char **/*argv*/) {
     viewer.data().clear();
     viewer.core.align_camera_center(P);
     viewer.data().point_size = 5;
-    viewer.data().add_points(P, Eigen::RowVector3d(0.f,0.7f,0.7f));
+    viewer.data().add_points(P1, Eigen::RowVector3d(0.f,0.7f,0.7f));
     viewer.data().set_mesh(mesh.vertices(), mesh.faces());
     viewer.data().compute_normals();
     viewer.data().invert_normals = true;
+    
+    igl::opengl::glfw::imgui::ImGuiMenu menu;
+    viewer.plugins.push_back(&menu);
+
+    menu.callback_draw_viewer_menu = [&]()
+    {
+      ImGui::Text("Iterate: \"1\"");
+      ImGui::Text("Next point cloud: \"2\"");
+    };
     
     viewer.launch();
 }
