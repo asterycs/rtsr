a3e30ef4dc8dac465e612875396ab2a4ae21a0cf
Sebastian <spam@blueapricot.ch>
2018-05-26 17:38:42 +0200

    Cherrypick Plane detection + reorientation

--- src/DataSet.hpp
+++ src/DataSet.hpp
@@ -5,6 +5,8 @@
 #include <boost/filesystem.hpp>
 
 #include "Eigen/Core"
+#include <Eigen/Geometry> 
+
 
 class DataSet
 {
@@ -13,16 +15,20 @@ public:
   ~DataSet();
   
   bool get_next_point_cloud(Eigen::MatrixXd& P, Eigen::MatrixXd &colors, Eigen::Matrix4d& t_camera);
+  Eigen::Affine3f findPlaneRotation(Eigen::MatrixXd& points);
+  void clip_point_clouds();
 private:
   bool get_next_camera(Eigen::Matrix4d& cam, const double timestamp);
   const char* get_next_rgb(const double timestamp);
 
   bool operational;
+  bool clip;
   std::vector<std::string> depth_files;
   int next_file_idx;
   std::string folder_path;
-  std::string camera_ref_file_name;
   std::string rgb_ref_file_name;
+  std::fstream camera_ref_file;
+  Eigen::Matrix4d rOffset;
 };
 
 #endif // DATASET_HPP
