2c67bdf724dea99e4e818a05bd4e681e75ed6a8d
Jimmy Envall <jimmyenvall@gmail.com>
2018-06-07 20:21:21 +0200

    Added error plot script.

---
+++ tools/error_plot.m
@@ -0,0 +1,20 @@
+clear all;
+close all;
+
+D = csvread('error.csv',1);
+
+PC_IDX = 0;
+LEVELS = 3;
+lgdstr = cell(1,LEVELS+1);
+
+for l=0:3
+    hold on;
+    R = D(D(:,2) == l & D(:,1) == PC_IDX,:);
+    plot(R(:,4));
+    grid on;
+    lgdstr{l+1} = sprintf('Residual level %d',l);
+end
+
+lgd = legend(lgdstr);
+lgd.FontSize = 14;
+
